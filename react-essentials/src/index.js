53271f20b2d77ee056b07e61f5134cae119ed18d
David Montanez <dmontanez@aceproject.space>
2022-06-18 13:45:17 -0500

    Refactoring elements using JSX

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -6,11 +6,29 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement(
-    "h1",
-    { style: { color: "blue" } },
-    "Heyyyyyy Everyone!"
-  ),
+  // React.createElement(
+  //   "h1",
+  //   { style: { color: "blue" } },
+  //   "Heyyyyyy Everyone!"
+  // ),
+
+  // React.createElement(
+  //   "ul",
+  //   null,
+  //   React.createElement("li", null, "Monday"),
+  //   React.createElement("li", null, "Tuesday"),
+  //   React.createElement("li", null, "Wednesday"),
+  //   React.createElement("li", null, "Thursday"),
+  //   React.createElement("li", null, "Friday"),
+  // ),
+
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+    <li>Thursday</li>
+    <li>Friday</li>
+  </ul>
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
