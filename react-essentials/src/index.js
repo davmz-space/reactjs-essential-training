3929d6918455cebae9080cdf7a42fa9005d460d7
David Montanez <dmontanez@aceproject.space>
2022-06-18 14:40:48 -0500

    Using fragments
    2 components need to be wrapped around a single tag in order for it to display

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -4,8 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function AppTwo() {
+  return (
+    <h1>This is the Second App</h1>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <>
+  //   <App />
+  //   <AppTwo />    
+  // </>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
