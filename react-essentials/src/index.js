22b8e52afe9275c3a306b28f218bd2fa996711ba
David Montanez <dmontanez@aceproject.space>
2022-06-18 13:42:20 -0500

    Create react element
    h1 element will be displayed on the localhost

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -6,9 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(
+    "h1",
+    { style: { color: "blue" } },
+    "Heyyyyyy Everyone!"
+  ),
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
